659d5d9d5e7342d3c2f5cedda983cf209c68c1b9
Eric Valderas <antikampa@gmail.com>
2026-09-07 09:18:17 +0200

    redireccion

--- scriptindex.js
+++ scriptindex.js
@@ -1,14 +1,14 @@
 
 
-document.getElementById("worldImage1").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };  
-document.getElementById("worldImage2").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage3").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage4").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage5").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage6").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage7").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage8").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };   
-document.getElementById("worldImage9").onclick = function() {window.open("https://ericvalderas.github.io/antidrogaradio/world.html");  };    
+document.getElementById("worldImage1").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };  
+document.getElementById("worldImage2").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage3").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage4").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage5").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage6").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage7").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage8").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
+document.getElementById("worldImage9").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };    
 
 const audioElement = document.querySelector("audio");
 var song = document.getElementById("myAudio");
