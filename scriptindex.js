433189d45a7ea938e3e511807870f29b224e0ad1
Eric Valderas <antikampa@gmail.com>
2026-09-08 18:46:09 +0200

    self tab

--- scriptindex.js
+++ scriptindex.js
@@ -1,23 +1,23 @@
 
 
-document.getElementById("worldImage1").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };  
-document.getElementById("worldImage2").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage3").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage4").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage5").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage6").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage7").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage8").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  };   
-document.getElementById("worldImage9").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html");  }; 
+document.getElementById("worldImage1").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };  
+document.getElementById("worldImage2").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage3").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage4").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage5").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage6").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage7").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage8").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  };   
+document.getElementById("worldImage9").onclick = function() {window.open("https://ericvalderas.github.io/nwr/world.html","_self");  }; 
 
-document.getElementById("punkImage1").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage2").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage3").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage4").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage5").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage6").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage7").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
-document.getElementById("punkImage8").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html");  }; 
+document.getElementById("punkImage1").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage2").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage3").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage4").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage5").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage6").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage7").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
+document.getElementById("punkImage8").onclick = function() {window.open("https://ericvalderas.github.io/nwr/punk.html","_self");  }; 
 
 const audioElement = document.querySelector("audio");
 var song = document.getElementById("myAudio");
