c3dc3d8f54c95bf3a5385f1e8ac1306c81bfb02e
Eric Valderas <antikampa@gmail.com>
2026-09-11 17:02:16 +0200

    20s

--- scriptindex.js
+++ scriptindex.js
@@ -40,7 +40,7 @@ window.addEventListener('load', () => {
   loader.style.opacity = '0';
   setTimeout(() => {
     loader.style.display = 'none';
-  }, 15000); // Coincide con la transición de CSS
+  }, 20000); // Coincide con la transición de CSS
 });
 
 
